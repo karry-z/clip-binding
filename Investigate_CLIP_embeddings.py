dd2612a459ab1a53c97133d9af6ad70454102737
pu22650 <pu22650@bp1-login01.data.bp.acrc.priv>
2023-06-23 20:47:15 +0100

    sperate pos and neg to aRb bRa aSb cRb aRc

--- Investigate_CLIP_embeddings.py
+++ Investigate_CLIP_embeddings.py
@@ -3,11 +3,11 @@
 # Lib
 import torch
 from train import *
-from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
-import matplotlib.colors as mcolors
+
+
+
 
 # Config
 class Config:
@@ -17,8 +17,8 @@ class Config:
     weight_decay = 1e-05
     clip_model = "ViT-L/14"
     epochs = 20
-    train_batch_size = 4
-    eval_batch_size = 4
+    train_batch_size = 1
+    eval_batch_size = 1
     gradient_accumulation_steps = 1
     evaluate_only = False
     context_length = 77
@@ -29,65 +29,32 @@ class Config:
     seed = 0
 
 config = Config()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Data
-dataset = choose_dataset(config)
-model, optimizer = get_model(dataset["train"], config, device)
-
-dataloader = DataLoader(dataset['val'], batch_size=config.eval_batch_size, shuffle=False)
-
-# Inference
-pos_mean = []
-neg_mean = []
-
-model.eval()
-with torch.no_grad():
-    for i, (batch_img, texts, labels) in enumerate(dataloader):
-        logits_per_image = model(batch_img.to(device), texts)
-        pos, neg = [],[]
-        for raw in range(len(labels)):
-            pos.append(logits_per_image[raw, labels[raw]])
-            for col in range(5):
-                if col != labels[raw]:
-                    neg.append(logits_per_image[raw, col])
-        pos = torch.stack(pos)
-        neg = torch.stack(neg)
-        # compute the mean value of pos and neg in each batch
-        pos_mean.append(pos.mean().item())
-        neg_mean.append(neg.mean().item())
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# save pos_mean and neg_mean in csv
-df = pd.DataFrame({'pos_mean': pos_mean, 'neg_mean': neg_mean})
-df.to_csv('clip_embeddings_val.csv', index=False)
 
 
-# def normal_dist(x, mu, sigma):
-#     return np.exp(-(x-mu)**2/(2*sigma**2))/np.sqrt(2*np.pi*sigma)
 
-# # df = pd.read_csv('clip_embeddings.csv')
-# X = np.linspace(15, 25, 500)
-# fig, ax = plt.subplots()
-# ax.set_title("distribution of sample mean of CLIP embeddings")
-# hist_neg = ax.hist(df['neg_mean'], bins='auto', density=True, label='neg hist', alpha = 0.3, color=mcolors.CSS4_COLORS['blue'])
-# hist_pos = ax.hist(df['pos_mean'], bins='auto', density=True, label='pos hist', alpha = 0.3, color=mcolors.CSS4_COLORS['green'])
-# neg_dist = stats.rv_histogram(hist_neg[:2])
-# pos_dist = stats.rv_histogram(hist_pos[:2])
+def get_distance_sample_mean(model, dataset, device):
+    df = pd.DataFrame(columns=['aRb', 'bRa', 'aSb', 'cRb', 'aRc'])
+    dataloader = DataLoader(dataset, batch_size=config.eval_batch_size, shuffle=False)
+    model.eval()
+    with torch.no_grad():
+        for i, (batch_img, texts, label) in enumerate(dataloader):
+            logits_per_image = model(batch_img.to(device), texts)
+            aRb = logits_per_image[:, 0].mean().item()
+            bRa = logits_per_image[:, 1].mean().item()
+            aSb = logits_per_image[:, 2].mean().item()
+            cRb = logits_per_image[:, 3].mean().item()
+            aRc = logits_per_image[:, 4].mean().item()
+            df.loc[i] = [aRb, bRa, aSb, cRb, aRc]
+    return df
 
-# ax.plot(X, normal_dist(X, neg_dist.mean(), neg_dist.std()), label='pdf of neg', color=mcolors.CSS4_COLORS['blue'])
-# ax.vlines(neg_dist.mean(), 0, 3, colors=mcolors.CSS4_COLORS['darkblue'], label='neg mean', linestyle='--')
-# print(neg_dist.mean(), neg_dist.std())
 
-# ax.plot(X, normal_dist(X, pos_dist.mean(), pos_dist.std()), label='pdf of pos', color=mcolors.CSS4_COLORS['green'])
-# ax.vlines(pos_dist.mean(), 0, 3, colors=mcolors.CSS4_COLORS['darkgreen'], label='pos mean', linestyle='--')
-# print(pos_dist.mean(), pos_dist.std())
-# ax.legend()
-# ax.set_xlabel('sample mean')
-# ax.set_ylabel('probability density')
-# ax.set_xlim(19, 23)
-# ax.set_ylim(0, 1)
-# fig.show()
 
-# stats.ttest_ind(a=df['neg_mean'], b=df['pos_mean']) 
-
+if __name__ == '__main__':
+    dataset = choose_dataset(config)
+    model, optimizer = get_model(dataset["train"], config, device)
+    for split in ["train", "val", "gen"]:
+        df = get_distance_sample_mean(model, dataset[split], device)
+        df.to_csv(f'clip_embeddings_distance_sample_mean_{split}.csv', index=False)
